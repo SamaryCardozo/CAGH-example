7fa4537ad4f0c086d609579a2efb51a0d56f44b5
SamaryCardozo <sycardozoc@upn.edu.co>
2023-07-12 17:58:50 -0500

    Probando primer ejercicio en POO

---
+++ Semana1/Clase1/ClaseOne/src/claseone/ClaseOne.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package claseone;
+
+/**
+ *
+ * @author samar
+ */
+public class ClaseOne {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         String homerImage = 
+            "       d'**b\n" +
+            "      :\"\"##%%.\n" +
+            "    :\"######%%\n" +
+            "   /#########\n" +
+            "  /##########\n" +
+            " |############\\n" +
+            "|##############\\n" +
+            "|##############|\n" +
+            "|##############|\n" +
+            "|##############|\n" +
+            "|##############|\n" +
+            "|##############|\n" +
+            "|#############/\n" +
+            " \\############/\n" +
+            "  |############|\n" +
+            "  |############|\n" +
+            "  |############|\n" +
+            "  |############|\n" +
+            "  |############|\n" +
+            "  \\############\\\n" +
+            "   \\##############\n" +
+            "    \\#############\n" +
+            "     \"######%%\"\"\n" +
+            "      ^\"\"##%%\"\n" +
+            "          d\"\"`b\n";
+        
+        System.out.println(homerImage);
+    }
+    
+}
